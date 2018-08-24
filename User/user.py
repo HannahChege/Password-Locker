db69c8a2c8c57c8d98a20b166b06b1a89ea73ba8
HannahChege <chegehannah45@gmail.com>
2018-08-24 17:13:48 +0300

    Added the delete function

--- User/user.py
+++ User/user.py
@@ -30,3 +30,18 @@ class User:
             '''
 
             User.user_list.remove(self)
+
+        @classmethod
+        def find_by_number(cls, number):
+            '''
+            Method that takes in a number and returns a contact that matches that number.
+
+            Args:
+                number: Phone number to search for
+            Returns :
+                Contact of person that matches the number.
+            '''
+
+            for user in cls.user_list:
+                if contact.phone_number == number:
+                    return user
