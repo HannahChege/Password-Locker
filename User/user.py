c769b7be623c0eb6eaf515fa5f1c0fa09f80695a
HannahChege <chegehannah45@gmail.com>
2018-08-25 08:57:30 +0300

    addede the copy and paste function

--- User/user.py
+++ User/user.py
@@ -57,3 +57,8 @@ class User:
             method that returns the user list
             '''
             return cls.user_list
+
+        @classmethod
+        def copy_email(cls, number):
+            user_found = User.find_by_number(number)
+            pyperclip.copy(user_found.email)
